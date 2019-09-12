6dd0ffb5c05628105341482e9c789462904d29d9
And Morita <anderson.morita@gmail.com>
2019-09-12 11:03:45 +0000

    my first commit

---
+++ webapp/controller/NotFound.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+		"zbopf/ZProjectTestBopf/controller/BaseController"
+	], function (BaseController) {
+		"use strict";
+
+		return BaseController.extend("zbopf.ZProjectTestBopf.controller.NotFound", {
+
+			/**
+			 * Navigates to the worklist when the link is pressed
+			 * @public
+			 */
+			onLinkPressed : function () {
+				this.getRouter().navTo("worklist");
+			}
+
+		});
+
+	}
+);
